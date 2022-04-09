fb703d156fd7160d74f82d2d255c34d62eebd458
Anne-Stine Gjervoldstad <annestine.kodehode@gmail.com>
2022-04-09 21:00:59 +0200

    Update html, css and js

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const navToggle = document.querySelector(".nav-toggle")
-const mobileMenu = document.querySelector(".mobile-menu")
-
-const toggleMobileMenu = (e) =>
+/* Make active nav item correspont with section (ID) in viewport */
+const activeNavState = () => 
 {
-    mobileMenu.className === "mobile-menu" 
-    ? mobileMenu.className += " active"
-    : mobileMenu.className = "mobile-menu"
+    /* Create an observer */
+    const observer = new IntersectionObserver(entries => 
+    {
+        /* For each section (ID) in viewport, adjust nav link's class to correspond with target */
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute("id")
+            if (entry.intersectionRatio > 0) {
+                document.querySelector(`.nav-link a[href="#${id}"]`).classList.add("active")
+            } else {
+                document.querySelector(`.nav-link a[href="#${id}"]`).classList.remove("active")
+            }
+        })
+    })
+    /* Observe all sections */
+    document.querySelectorAll("section[id]").forEach((section) => {
+        observer.observe(section)
+    })
 }
+window.addEventListener("DOMContentLoaded", activeNavState)
 
-navToggle.addEventListener("click", toggleMobileMenu)
+/* Switch to mobile menu when screen width is less than 650px */
+const toggleNavType = () => 
+{
+    /* Screen width */
+    let screenWidth = document.documentElement.clientWidth
+
+    if (screenWidth < 650) {
+        document.querySelector(".nav-items").setAttribute("id", "mobile-nav")
+    } else {
+        document.querySelector(".nav-items").setAttribute("id", "")
+    }
+}
+/* Check screen width on page load and resize */
+window.addEventListener("DOMContentLoaded", toggleNavType)
+window.addEventListener("resize", (e) => toggleNavType(e))
